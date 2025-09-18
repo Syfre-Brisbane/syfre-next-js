6e3cc15457a09553f6318c27addc961c6e3fd648
Steve Macfarlane <stevemacfarlane@MacBook-Air-3.local>
2025-09-18 16:07:34 +1000

    remove contact details

--- src/components/Footer/Links.tsx
+++ src/components/Footer/Links.tsx
@@ -2,11 +2,7 @@ export default function Links() {
   return (
     <div className="flex flex-col gap-1 text-sm">
       <h3 className="font-semibold text-white mb-1">Syfre</h3>
-      <p className="font-light text-white">0414 383 094</p>
       <p className="font-normal text-green-400">info@syfre.com.au</p>
-      <p className="font-light text-white">
-        40 Prospect Street, Fortitude Valley, QLD 4006
-      </p>
     </div>
   );
 }
