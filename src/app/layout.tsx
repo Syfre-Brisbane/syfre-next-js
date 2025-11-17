923ee05345bcc4a9eeeb6409fd1883c82a494f6f
Steve Macfarlane <stevemacfarlane@MacBook-Air-3.local>
2025-11-17 15:37:45 +1000

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const sourceSans3 = Source_Sans_3({
@@ -20,6 +21,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y8T93PQ37E"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Y8T93PQ37E');
+        `}
+      </Script>
       <body
         className={`${sourceSans3.variable} antialiased font-sans`}
       >
